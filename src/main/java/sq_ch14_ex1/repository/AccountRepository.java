5ab3c36c4acc1a4c7552074a25b4e51a24ebf870
osparking <jbpark03@gmail.com>
2023-11-06 20:39:55 +0900

    질의어가 명세된 계좌 저장소 메소드

--- src/main/java/sq_ch14_ex1/repository/AccountRepository.java
+++ src/main/java/sq_ch14_ex1/repository/AccountRepository.java
@@ -1,9 +1,13 @@
 package sq_ch14_ex1.repository;
 
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import sq_ch14_ex1.model.Account;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
-
+	@Query("select * from account where name = :name")
+	List<Account> findByName(String name);
 }
