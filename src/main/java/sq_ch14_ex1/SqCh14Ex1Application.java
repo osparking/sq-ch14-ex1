48c1e47c4b5fbc2e644f6022a041e58bc92b60c1
osparking <jbpark03@gmail.com>
2023-11-06 12:29:19 +0900

    종속성: lombok, web, data-jdbc, h2

---
+++ src/main/java/sq_ch14_ex1/SqCh14Ex1Application.java
@@ -0,0 +1,13 @@
+package sq_ch14_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh14Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh14Ex1Application.class, args);
+	}
+
+}
---
+++ src/test/java/sq_ch14_ex1/SqCh14Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch14_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh14Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
