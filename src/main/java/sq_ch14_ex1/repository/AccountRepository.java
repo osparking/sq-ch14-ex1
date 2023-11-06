32e4e0e07ec92286140fd076e837b93e82e8b750
osparking <jbpark03@gmail.com>
2023-11-06 20:48:41 +0900

    저장소 인터' 메소드 중 자료 변경하는 경우

--- src/main/java/sq_ch14_ex1/repository/AccountRepository.java
+++ src/main/java/sq_ch14_ex1/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package sq_ch14_ex1.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,4 +12,8 @@ import sq_ch14_ex1.model.Account;
 public interface AccountRepository extends CrudRepository<Account, Long> {
 	@Query("select * from account where name = :name")
 	List<Account> findByName(String name);
+	
+	@Query("update account set amount = :amount where id = :id ")
+	@Modifying
+	void changeAmount(long id, BigDecimal amount);
 }
