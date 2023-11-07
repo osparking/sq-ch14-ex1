63aa35a62bc4a31ffa1bc44910b6e51365f165e8
osparking <jbpark03@gmail.com>
2023-11-07 11:24:44 +0900

    두 검색 메소드

--- src/main/java/sq_ch14_ex1/service/TransferService.java
+++ src/main/java/sq_ch14_ex1/service/TransferService.java
@@ -1,10 +1,13 @@
 package sq_ch14_ex1.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 import sq_ch14_ex1.exception.AccountNotFoundException;
+import sq_ch14_ex1.model.Account;
 import sq_ch14_ex1.model.TransferRequest;
 import sq_ch14_ex1.repository.AccountRepository;
 
@@ -26,6 +29,14 @@ public class TransferService {
 
 		repository.changeAmount(request.getSenderId(), senderAmt);
 		repository.changeAmount(request.getReceiverId(), receiverAmt);
+	}
+	
+	public Iterable<Account> getAllAccounts() {
+		return repository.findAll();
+	}
+	
+	public List<Account> findByName(String name) {
+		return repository.findByName(name);
 	}
 
 }
