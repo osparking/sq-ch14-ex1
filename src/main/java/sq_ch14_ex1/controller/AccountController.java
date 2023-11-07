964feac405499a1c9769a4899bde3cd50238b13c
osparking <jbpark03@gmail.com>
2023-11-07 13:06:01 +0900

    모든 계좌 읽기 - 선택적 예금주 명

--- src/main/java/sq_ch14_ex1/controller/AccountController.java
+++ src/main/java/sq_ch14_ex1/controller/AccountController.java
@@ -1,10 +1,13 @@
 package sq_ch14_ex1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import sq_ch14_ex1.model.Account;
 import sq_ch14_ex1.model.TransferRequest;
 import sq_ch14_ex1.service.TransferService;
 
@@ -16,5 +19,16 @@ public class AccountController {
 	@PostMapping("/transfer")
 	public void transferMoney(@RequestBody TransferRequest request) {
 		service.transferMoney(request);	
+	}
+	
+	@GetMapping("/accounts")
+	public Iterable<Account> getAllAccounts(
+			@RequestParam(required = false) String name) {
+		if (name == null) {
+			return service.getAllAccounts();
+		} else {
+			return service.findByName(name);
+		}
+		
 	}
 }
